b9e98043b24106bfc713db435de61be6cb1d37e3
vamsynani <130791616+vamsynani@users.noreply.github.com>
2023-05-06 18:06:57 +0530

    Update script.js

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,5 +1,5 @@
  let container = document.querySelector('.container');
-      let url = 'https://source.unsplash.com/developers';
+      let url = 'https://source.unsplash.com/random';
       
       for (let index = 0; index < 5 * 8; index++) {
         let img = document.createElement('img');
@@ -7,5 +7,5 @@
         img.src = `${url}/${index}`;
         container.appendChild(img);
       }
-      fetch('https://source.unsplash.com/developers')
+      fetch('https://source.unsplash.com/random')
       .then()
